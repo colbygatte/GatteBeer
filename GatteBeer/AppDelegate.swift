5f8359fa7e6b7619958a928bc9df94aec5264be9
colbygatte <colbygatte@gmail.com>
2016-12-28 15:45:07 -0600

    First commit.

---
+++ GatteBeer/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GatteBeer
+//
+//  Created by Colby Gatte on 12/28/16.
+//  Copyright © 2016 colbyg. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-GatteBeer/Pods-GatteBeer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GatteBeer : NSObject
+@end
+@implementation PodsDummy_Pods_GatteBeer
+@end
